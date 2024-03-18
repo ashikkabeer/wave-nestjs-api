375b3beb71de52cdc2702aeb8fd08c704faa8573
Ashik Kabeer <97529548+ashikkabeer@users.noreply.github.com>
2024-03-18 21:46:35 +0530

    create chat message feature

--- src/message/dto/create-message.dto.ts
+++ src/message/dto/create-message.dto.ts
@@ -1,14 +1,14 @@
-import { isNotEmpty, isString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 export class CreateMessageDto {
-  @isNotEmpty()
-  @isString()
+  @IsString()
+  @IsNotEmpty()
   content: string;
 
-  @isString()
-  @isNotEmpty()
+  @IsString()
+  @IsNotEmpty()
   chatId: string;
 
-  @isString()
-  @isNotEmpty()
+  @IsString()
+  @IsNotEmpty()
   userId: string;
 }
--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -1,10 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ForbiddenException } from '@nestjs/common';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class MessageService {
-  create(createMessageDto: CreateMessageDto) {
+  constructor(
+    private prisma: PrismaService,
+    private config: ConfigService,
+    private jwt: JwtService,
+  ) {}
+
+  async create(createMessageDto: CreateMessageDto) {
+    try {
+      const message = await this.prisma.message.create({
+        data: {
+          content: createMessageDto.content,
+          chatId: createMessageDto.chatId,
+          userId: createMessageDto.userId,
+        },
+      });
+      return message;
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        // * p2002 -> duplication
+        if (error.code == 'P2002') {
+          throw new ForbiddenException('Credentials Already Taken');
+        }
+      }
+      throw error;
+    }
     return 'This action adds a new message';
   }
 
