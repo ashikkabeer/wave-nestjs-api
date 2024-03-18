20636833a2cdd50cdb71fedad6a3117a59d1df84
Ashik Kabeer <97529548+ashikkabeer@users.noreply.github.com>
2024-03-18 21:31:58 +0530

    chat feature schema

--- src/message/dto/create-message.dto.ts
+++ src/message/dto/create-message.dto.ts
@@ -1,1 +1,14 @@
-export class CreateMessageDto {}
+import { isNotEmpty, isString } from 'class-validator';
+export class CreateMessageDto {
+  @isNotEmpty()
+  @isString()
+  content: string;
+
+  @isString()
+  @isNotEmpty()
+  chatId: string;
+
+  @isString()
+  @isNotEmpty()
+  userId: string;
+}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -12,4 +12,7 @@ export class PrismaService extends PrismaClient {
       },
     });
   }
+  cleanDb() {
+    return this.$transaction([]);
+  }
 }
